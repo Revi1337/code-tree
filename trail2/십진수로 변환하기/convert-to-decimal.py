ea61d30c0370097b41ab485d193e327ba58a73fa
Revi1337 <david122123@gmail.com>
2026-08-02 23:16:53 +0900

    [Passed] 십진수로 변환하기 (48ms, 16MB)

--- trail2/십진수로 변환하기/convert-to-decimal.py
+++ trail2/십진수로 변환하기/convert-to-decimal.py
@@ -1,10 +1,31 @@
 def solution(binary):
-    ans, sbin = 0, str(binary)
+    ans, sbin = 0, str(binary)[::-1]
     for idx in range(len(sbin)):
         if sbin[idx] == '1':
-            cost = 2 ** (len(sbin) - 1 - idx)
+            cost = 2 ** idx
             ans += cost
     return ans
 
 binary = int(input())
 print(solution(binary))
+
+
+# import sys
+#
+# sys.stdin = open('input.txt', 'r', encoding='utf-8')
+# input = sys.stdin.readline
+#
+# """
+# https://www.codetree.ai/ko/trails/complete/curated-cards/intro-convert-to-decimal/description
+# """
+#
+# def solution(binary):
+#     ans, sbin = 0, str(binary)
+#     for idx in range(len(sbin)):
+#         if sbin[idx] == '1':
+#             cost = 2 ** (len(sbin) - 1 - idx)
+#             ans += cost
+#     return ans
+#
+# binary = int(input())
+# print(solution(binary))
