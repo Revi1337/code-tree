ba799be0a0fc883974a7ff5fa4265d8f19aff1b0
Revi1337 <david122123@gmail.com>
2026-08-02 21:28:54 +0900

    [Passed] DateTime to DateTime (51ms, 16MB)

--- trail2/DateTime to DateTime/datetime-to-datetime.py
+++ trail2/DateTime to DateTime/datetime-to-datetime.py
@@ -2,9 +2,8 @@ D, H, M = 11, 11, 11
 
 def solution(a, b, c):
     ans = ((a - D) * 24 * 60) + ((b - H) * 60) + (c - M)
-    return ans if ans > 0 else -1
+    return -1 if ans < 0 else ans
 
 a, b, c = map(int, input().split())
 print(solution(a, b, c))
-
 
