e92973431031a39e650745b11c566a77f77827c2
Revi1337 <david122123@gmail.com>
2026-08-03 16:30:07 +0900

    [Wrong Answer] 왔다 갔던 구역 2 (63ms, 16MB)

---
+++ trail2/왔다 갔던 구역 2/area-been-to-and-from2.py
@@ -0,0 +1,17 @@
+def solution(n, segments):
+    arr = [0] * ((100 * 10 * 2) + 1)
+    curr = (100 * 10 * 2) // 2
+    for dist, dir in segments:
+        dist = int(dist)
+        if dir == 'L':
+            for v in range(curr, curr - dist, -1):
+                arr[v] += 1
+        else:
+            for v in range(curr, curr + dist):
+                arr[v] += 1
+
+    return max(arr)
+
+n = int(input())
+segments = [list(input().split()) for _ in range(n)]
+print(solution(n, segments))
