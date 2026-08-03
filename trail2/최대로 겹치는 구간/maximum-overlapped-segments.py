79f145fc6ad0f67f8e8e66f9e230d50643a4416e
Revi1337 <david122123@gmail.com>
2026-08-03 16:03:33 +0900

    [Passed] 최대로 겹치는 구간 (58ms, 17MB)

---
+++ trail2/최대로 겹치는 구간/maximum-overlapped-segments.py
@@ -0,0 +1,16 @@
+def solution(n, commands):
+    off = min(commands, key=lambda x: x[0])[0]
+    if off > 0:
+        commands = [[x1 + off, x2 + off] for x1, x2 in commands]
+
+    arr = [0] * 201
+    for v1, v2 in commands:
+        if v1 <= v2:
+            for v in range(v1, v2):
+                arr[v] += 1
+
+    return max(arr)
+
+n = int(input())
+commands = [tuple(map(int, input().split())) for _ in range(n)]
+print(solution(n, commands))
