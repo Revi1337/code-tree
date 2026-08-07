19c95dd3a1462baa7826facd5f6e79b3ffcdaed6
Revi1337 <david122123@gmail.com>
2026-08-07 23:24:50 +0900

    [Wrong Answer] 연속되는 수 2 (47ms, 16MB)

--- trail2/연속되는 수 2/continuous-number2.py
+++ trail2/연속되는 수 2/continuous-number2.py
@@ -1,1 +1,14 @@
+def solution(N, arr):
+    ans = 0
+    length = 1
+    for idx in range(N):
+        if idx == 0 or arr[idx - 1] != arr[idx]:
+            ans = max(ans, length)
+            length = 1  
+        else:
+            length += 1
+    return ans
+        
 N = int(input())
+arr = [int(input()) for _ in range(N)]
+print(solution(N, arr))
