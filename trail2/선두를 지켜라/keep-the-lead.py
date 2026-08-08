08fd3861e1542207c009df847fa8832cd934db62
Revi1337 <david122123@gmail.com>
2026-08-08 19:01:51 +0900

    [Runtime Error] 선두를 지켜라 (96ms, 19MB)

---
+++ trail2/선두를 지켜라/keep-the-lead.py
@@ -0,0 +1,33 @@
+def solution(n, m, A, B):
+    arr1, arr2 = [[0] * 1001 for _ in range(2)]
+
+    ct = cv = 0
+    for v, t in A:
+        while t > 0:
+            t -= 1
+            cv += v
+            ct += 1
+            arr1[ct] = cv
+    ct = cv = 0
+    for v, t in B:
+        while t > 0:
+            t -= 1
+            cv += v
+            ct += 1
+            arr2[ct] = cv
+
+    diffs = []
+    for t in range(1, len(arr1)):
+        if arr1[t] - arr2[t]:
+            diffs.append(arr1[t] - arr2[t])
+
+    ans = 0
+    for t in range(1, len(diffs)):
+        if not ((diffs[t] > 0 and diffs[t - 1] > 0) or (diffs[t] < 0 and diffs[t - 1] < 0)):
+            ans += 1
+    return ans
+
+n, m = map(int, input().split())
+A = [list(map(int, input().split())) for _ in range(n)]
+B = [list(map(int, input().split())) for _ in range(m)]
+print(solution(n, m, A, B))
